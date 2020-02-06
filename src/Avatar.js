963091273db24452bed942cc74920d90c6018ee0
Jeserlin <jeserlin@me.com>
2020-02-06 10:42:48 +0800

    Test

--- src/Avatar.js
+++ src/Avatar.js
@@ -98,6 +98,26 @@ const Initial = styled.div`
   `}
 `;
 
+const props = {
+  /**
+    Use the loading state to indicate that the data Avatar needs is still loading. 1
+    */
+  loading: PropTypes.bool,
+  /**
+    Avatar falls back to the user's initial when no image is provided. 
+    Supply a `username` and omit `src` to see what this looks like.
+    */
+  username: PropTypes.string,
+  /**
+    The URL of the Avatar's image.
+    */
+  src: PropTypes.string,
+  /**
+    Avatar comes in four sizes. In most cases, you'll be fine with `medium`.
+    */
+  size: PropTypes.oneOf(Object.keys(sizes))
+}
+
 /**
 - This comment is added in js
 - Use an avatar for attributing actions or content to specific users.
@@ -128,25 +148,7 @@ export function Avatar({ loading, username, src, size, ...props }) {
   );
 }
 
-Avatar.propTypes = {
-  /**
-    Use the loading state to indicate that the data Avatar needs is still loading.
-    */
-  loading: PropTypes.bool,
-  /**
-    Avatar falls back to the user's initial when no image is provided. 
-    Supply a `username` and omit `src` to see what this looks like.
-    */
-  username: PropTypes.string,
-  /**
-    The URL of the Avatar's image.
-    */
-  src: PropTypes.string,
-  /**
-    Avatar comes in four sizes. In most cases, you'll be fine with `medium`.
-    */
-  size: PropTypes.oneOf(Object.keys(sizes))
-};
+Avatar.propTypes = props;
 
 Avatar.defaultProps = {
   loading: false,
